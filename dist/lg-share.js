2b599f9c0728c9aae56d267c67212afda2517dfa
sachin <sachi77n@gmail.com>
2016-08-01 09:12:14 +0530

    Dist

--- dist/lg-share.js
+++ dist/lg-share.js
@@ -1,5 +1,5 @@
 /**!
- * lg-share.js | 0.0.1 | July 30th 2016
+ * lg-share.js | 0.0.1 | August 1st 2016
  * http://sachinchoolur.github.io/lg-share.js
  * Copyright (c) 2016 Sachin N; 
  * @license Apache 2.0 
